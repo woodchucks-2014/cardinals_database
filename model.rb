cc687f059a03ca9fb24fb775ec7e9b7ac290a65c
Ian Shuff <ianvshuff@gmail.com>
2014-06-13 20:56:43 -0400

    Added mapping method. CSV maps correctly, to hash atleast.

--- model.rb
+++ model.rb
@@ -2,18 +2,36 @@ require 'csv'
 
 module CSV_Transfer
 
-
-
-
-
-	def self.csv_turn_to_array
+	def self.format_array 
 		array = []
-		CSV.foreach('nation_league_wins.csv', 'r') do |file| 
-			array << file
+		CSV.foreach('nation_league_wins.csv', 'r'){ |file| array << file}
+		teams = array[0][2..-1]
+		years = array.transpose[0][1..-1]
+		data = array.map!{|sub_array| sub_array[2..-1]}[1..-1]
+		# data.each {|line| print line; puts }
+		data.map!.with_index do |year, year_index| 
+			year.map!.with_index do |score, index| 
+			score = {teams[index] => score}
+			end
+			year = {years[year_index] => year}
 		end
-	array.each{|line| print line; puts} 
+		 data.each {|line| print line; puts}
+# 		p data
+# 		teams.each_with_index do |team, index|
+# 			team = {team => data[0][index]}
+# 			print team 
+# 		end
+# puts 
+
+		# data.each_with_index do |line, index|
+		# 	print line.unshift(years[index])
+		# 	puts
+		# end
+
+
 	end
+
 
 end
 
-CSV_Transfer.csv_database
+CSV_Transfer.format_array
