8edb288189809ae884854d05b9f6689a99e403be
Tanner Welsh <tannerwelsh@gmail.com>
2014-06-16 11:21:40 -0400

    Refactor Model & repair View: - Namespace w/in CardinalsDatabase - Remove CSV dependency - Use prepared SQL statements - Fix string conversion errors in View - Replace tabs with spaces in View

--- model.rb
+++ model.rb
@@ -1,56 +1,50 @@
-require 'csv'
-require 'sqlite3'
+require './config'
 
-module DbFactory #Implement the DB Factory
-  def self.make_db(file)
-    SQLite3::Database.new file
-  end
+module CardinalsDatabase
+  class Model
+    def team_year_lookup(team, year)
+      # Wins for a team and year
 
-  def self.csv_2_array(csv_path)
-    csv_array =[]
-    CSV.foreach(csv_path) do |csv|
-      csv_array << csv
+      sql = <<-SQL
+        SELECT stats.wins FROM stats
+        INNER JOIN teams
+        ON teams.id = stats.team_id
+        AND teams.name = ?
+        AND stats.year = ?;
+      SQL
+
+      CardinalsDatabase.db.execute(sql, team, year)[0][0]
     end
-    csv_array
-  end
 
-  def find_header(array)
-    array[0]
-  end
+    def team_win_totals(team, display = 0)
+      #Takes a Team and A Number 1-24(for display argument)
 
-end
+      range = 2014 - display
 
-class Model
-	include DbFactory
-  attr_reader :database, :nl_array
+      sql = <<-SQL
+        SELECT stats.year, stats.wins FROM stats
+        INNER JOIN teams
+        ON teams.id = stats.team_id
+        AND teams.name = ?
+        AND stats.year >= ?;
+      SQL
 
-  def initialize
-		@database = DbFactory.make_db('nl_real_wins.db')
-    @nl_array = DbFactory.csv_2_array("nation_league_wins.csv")
-	end
+      CardinalsDatabase.db.execute(sql, team, range)
+    end
 
+    def year_lookup(year, display=5)
+      #Takes a year and the number of teams you want to display.
 
-	def team_year_lookup(team, year)
-    #Takes Team and Year
-  	wins = @database.execute("select #{team} from nl_real_wins where YEAR = #{year}")
-	end
+      sql = <<-SQL
+        SELECT teams.name, stats.wins FROM stats
+        INNER JOIN teams
+        ON teams.id = stats.team_id
+        AND stats.year = ?
+        ORDER BY teams.name;
+      SQL
 
-	def team_win_totals(team, display = 0)
-    #Takes a Team and A Number 1-24(for display argument)
-    range = (2014 - display)
-    wins = @database.execute("SELECT #{team},YEAR FROM nl_real_wins WHERE Year >= #{range};")
-	end
-
-	def year_lookup(year, display=5)
-    #Takes a year and the number of teams you want to display.
-    wins = @database.execute("SELECT * from nl_real_wins WHERE YEAR = #{year}")
-    wins = wins.flatten[2..-1].map!{|value| value.to_i}
-    names = find_header(@nl_array)
-    wins_and_names = wins.zip(names).sort_by {|a, b| a}.reverse #This can be made clearer.
-    wins_and_names[0..(display -1)]
+      wins = CardinalsDatabase.db.execute(sql, year)
+      wins[0..(display-1)]
+    end
   end
 end
-
-# model = Model.new
-
-# p model.year_lookup(2014, 5)
--- view.rb
+++ view.rb
@@ -1,59 +1,58 @@
 class View
 
-	def welcome
-		puts "Welcome to the game! Would you like to play? Enter Y/N"
-		answer = gets.chomp
-	end
+  def welcome
+    puts "Welcome to the game! Would you like to play? Enter Y/N"
+    answer = gets.chomp
+  end
 
-	def selection
-		puts "1: Lookup Wins for a specific Year and Team:"
-		puts "2: Lookup Wins for a team with a range of specific years:"
-		puts "3: Lookup Winningest teams for a particular year: "
-		puts "4: Exit The Program"
-		gets.chomp
-	end
+  def selection
+    puts "1: Lookup Wins for a specific Year and Team:"
+    puts "2: Lookup Wins for a team with a range of specific years:"
+    puts "3: Lookup Winningest teams for a particular year: "
+    puts "4: Exit The Program"
+    gets.chomp
+  end
 
-	def ask_team
-		puts "Please Enter Your Team: "
-		gets.chomp
-	end
+  def ask_team
+    puts "Please Enter Your Team: "
+    gets.chomp
+  end
 
-	def ask_year
-		puts "Please Enter Your Year"
-		gets.chomp
-	end
+  def ask_year
+    puts "Please Enter Your Year"
+    gets.chomp
+  end
 
-	def ask_range
-		puts "Select your years of history for viewing:"
-		gets.chomp
-	end
+  def ask_range
+    puts "Select your years of history for viewing:"
+    gets.chomp
+  end
 
-	def team_number
-		puts "Please enter the number of teams for display:"
-		gets.chomp
-	end
+  def team_number
+    puts "Please enter the number of teams for display:"
+    gets.chomp
+  end
 
-	def team_wins(team, wins, year)
-		wins = wins.flatten.join
-		puts "#{team} won #{wins} games in #{year}."
-	end
+  def team_wins(team, wins, year)
+    puts "#{team} won #{wins} games in #{year}."
+  end
 
-	def year_wins(report)
-		puts "Year            Wins"
+  def year_wins(report)
+    puts "Year            Wins"
     puts "====================" #Look into better implementation here.
     report[1..-1].each do |report|
-    	puts report[1] + "              " + report[0]
+      puts report[1].to_s + "              " + report[0].to_s
     end
-	end
+  end
 
-	def year_lookup(output)
-		p output
-		puts "Year            Wins"
+  def year_lookup(output)
+    p output
+    puts "Year            Wins"
     puts "===================="
     output.each do |report|
-      puts report[0].to_s + "              " + report[1]
-		end
-	end
+      puts report[0].to_s + "              " + report[1].to_s
+    end
+  end
 
 
 end
